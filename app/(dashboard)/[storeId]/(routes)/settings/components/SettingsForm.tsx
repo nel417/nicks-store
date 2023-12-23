25a911fee3117b3180e1f095b5ebfe3c8a43db77
nickL <n1ck@hey.com>
2023-12-22 20:09:18 -0600

    patch route

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -13,7 +13,9 @@ import { Heading } from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
+import axios from "axios";
 import { Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -30,9 +32,19 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
-
+  const params = useParams()
+const router = useRouter()
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log(data);
+    try {
+        setLoading(true)
+        await axios.patch(`/api/stores/${params.storeId}`, data)
+        router.refresh()
+        console.log("store updated")
+    } catch (error) {
+        console.log("something went wrong", error)
+    } finally {
+        setLoading(false)
+    }
   };
   const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
